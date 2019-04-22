d24882847e47c837cb4da55159cb74082bf80196
wangnima <389483637@qq.com>
2019-04-22 22:17:13 +0800

    谷歌验证码v3

---
+++ Google.reCAPTCHA.V3.Example/Google.reCAPTCHA.V3.Example/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Google.reCAPTCHA.V3.Example.Models;
+using reCAPTCHA.AspNetCore;
+
+namespace Google.reCAPTCHA.V3.Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRecaptchaService _recaptcha;
+
+        public HomeController(IRecaptchaService recaptcha)
+        {
+            _recaptcha = recaptcha;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(AccountViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var recaptchaReault = await _recaptcha.Validate(model.GoogleToken);
+
+                if (!recaptchaReault.success || recaptchaReault.score < 0.08m)
+                 //if (!recaptchaReault.success || recaptchaReault.score<1)
+                {
+                    ModelState.AddModelError(string.Empty, "老实说，你是不是机器人！");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "登录成功~");
+                }
+            }
+
+            return View(model);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Google.reCAPTCHA.V3.Example/Google.reCAPTCHA.V3.Example/Models/AccountViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Google.reCAPTCHA.V3.Example
+{
+    public class AccountViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string GoogleToken { get; set; }
+    }
+}
