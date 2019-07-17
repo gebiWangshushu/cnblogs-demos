ab7bcbf07d51246b47fb8940926de9ffc9cb5cc0
王思恒 <wangsiheng@mail.39.net>
2019-07-17 23:04:20 +0800

    id4 SSO 学习

---
+++ IdentityServer4.Example/MvcImplicit/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace MvcImplicit
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            string port = config["port"];
+            IWebHostBuilder host = WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
+            if (!string.IsNullOrWhiteSpace(port))
+            {
+                host.UseUrls($"http://*:{port}");
+            }
+            return host;
+        }
+    }
+}
