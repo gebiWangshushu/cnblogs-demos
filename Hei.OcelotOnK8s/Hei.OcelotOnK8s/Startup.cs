38dfd6fd7e41d27cde2db8ef93a6c74d09f55f6a
wangnima <389483637@qq.com>
2020-05-27 23:46:07 +0800

    demo完成

--- Hei.OcelotOnK8s/Hei.OcelotOnK8s/Startup.cs
+++ Hei.OcelotOnK8s/Hei.OcelotOnK8s/Startup.cs
@@ -31,12 +31,13 @@ namespace Hei.OcelotOnK8s
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot();
+            services.AddOcelot(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseRouting();
             app.UseOcelot().Wait();
         }
     }
